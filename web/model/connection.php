ec71f0988c48f5c3758322ca1b4eccaef070803c
Henrique Bossle <163010167+HenriqueBossle@users.noreply.github.com>
2025-05-16 20:48:56 -0300

    Update connection.php

--- web/model/connection.php
+++ web/model/connection.php
@@ -3,8 +3,8 @@
 
 
 $hostname = "127.0.0.1";
-$username = "root";
-$password = "12345";
+$username = "";
+$password = "";
 $databank = "kaijin";
 
 $mysqli = new mysqli($hostname, $username, $password, $databank);
